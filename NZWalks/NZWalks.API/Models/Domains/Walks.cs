2e64aa5f1b8e396c4869c547a5b2b8f3bd9a7712
Saquib Hassan <hsakib1244@outlook.com>
2024-09-08 14:34:12 +0600

    MywalkInNZ

---
+++ NZWalks/NZWalks.API/Models/Domains/Walks.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.Domains
+{
+    public class Walks
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+
+    }
+}
