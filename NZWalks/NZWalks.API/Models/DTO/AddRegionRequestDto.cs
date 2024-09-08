14a9ff4c9380edc953e9f0f15c66be9e5b2be03f
Saquib Hassan <hsakib1244@outlook.com>
2024-09-08 19:45:47 +0600

    AddRegionRequestDto

--- NZWalks/NZWalks.API/Models/DTO/AddRegionRequestDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/AddRegionRequestDto.cs
@@ -2,6 +2,8 @@
 {
     public class AddRegionRequestDto
     {
-
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
     }
 }
