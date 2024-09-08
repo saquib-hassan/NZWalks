290c817001ffb744a02f5492be3148e36b07ca6e
Saquib Hassan <hsakib1244@outlook.com>
2024-09-08 15:01:20 +0600

    ctor

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
