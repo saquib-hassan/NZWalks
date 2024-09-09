af8300df6ea59f1c92cd9eed822083087831f11e
Saquib Hassan <hsakib1244@outlook.com>
2024-09-09 22:52:32 +0600

    craete walk

--- NZWalks/NZWalks.API/Repository/IWalkRepository.cs
+++ NZWalks/NZWalks.API/Repository/IWalkRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace NZWalks.API.Repository
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repository
 {
     public interface IWalkRepository
     {
-
+        Task<Walk>CreateAsync(Walk walk);
     }
 }
