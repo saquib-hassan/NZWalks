bf6ad8176e2df0e1b718b66eb55d0806c67ecff5
Saquib Hassan <hsakib1244@outlook.com>
2024-09-09 19:52:15 +0600

    Difficulty is populated

--- NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -18,6 +18,27 @@ namespace NZWalks.API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            var difficulties = new List<Difficulty>()
+            {
+                new Difficulty
+                {
+                    Id=Guid.Parse("a87e4c59-f395-4c65-a40a-7fea123374d0"),
+                    Name="Easy"
+                },
+                new Difficulty
+                {
+                    Id=Guid.Parse("7565b2bc-e2f9-4812-a9dc-8ff367820d58"),
+                    Name="Medium"
+                },
+                new Difficulty
+                {
+                    Id=Guid.Parse("42239bd9-c2f6-401c-8855-b62e1be64f68"),
+                    Name="Hard"
+                }
+            };
         }
+
+        
     }
 }
