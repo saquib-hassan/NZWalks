ed662488931f79c493641f45242449b4c17d19ae
Saquib Hassan <hsakib1244@outlook.com>
2024-09-09 12:57:56 +0600

    implemented

---
+++ NZWalks/NZWalks.API/Repository/SqlRegionRepository.cs
@@ -0,0 +1,12 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repository
+{
+    public class SqlRegionRepository : IRegionRepository
+    {
+        public Task<Region> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
