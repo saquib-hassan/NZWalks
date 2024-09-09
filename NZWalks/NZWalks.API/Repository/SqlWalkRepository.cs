ac06657268e50a3a4e814157c5fb421d9d8e06cc
Saquib Hassan <hsakib1244@outlook.com>
2024-09-09 22:53:46 +0600

    SqlWalkRepository

---
+++ NZWalks/NZWalks.API/Repository/SqlWalkRepository.cs
@@ -0,0 +1,12 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repository
+{
+    public class SqlWalkRepository : IWalkRepository
+    {
+        public Task<Walk> CreateAsync(Walk walk)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
