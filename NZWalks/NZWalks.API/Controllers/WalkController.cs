c0294343b56e7ab0f0315624b05cbd9f2dfa7d00
Saquib Hassan <hsakib1244@outlook.com>
2024-09-10 21:40:30 +0600

    if nullable then true

--- NZWalks/NZWalks.API/Controllers/WalkController.cs
+++ NZWalks/NZWalks.API/Controllers/WalkController.cs
@@ -39,9 +39,9 @@ namespace NZWalks.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetALl([FromQuery] string? filterOn, [FromQuery] string? filterQuery,[FromQuery]string? sortBy,[FromQuery] bool isAscending )
+        public async Task<IActionResult> GetALl([FromQuery] string? filterOn, [FromQuery] string? filterQuery,[FromQuery]string? sortBy,[FromQuery] bool? isAscending )
         {
-            var walkDomainModel = await walkRepository.GetAllAsync(filterOn,filterQuery,sortBy,isAscending);
+            var walkDomainModel = await walkRepository.GetAllAsync(filterOn,filterQuery,sortBy,isAscending?? true);
 
             return Ok(mapper.Map<List<WalkDto>>(walkDomainModel));
         }
