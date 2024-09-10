05df527b92b42df59b75789fdba8ce96804e5b34
Saquib Hassan <hsakib1244@outlook.com>
2024-09-10 15:20:42 +0600

    implemented

--- NZWalks/NZWalks.API/Repository/IWalkRepository.cs
+++ NZWalks/NZWalks.API/Repository/IWalkRepository.cs
@@ -7,6 +7,6 @@ namespace NZWalks.API.Repository
         Task<Walk>CreateAsync(Walk walk);
         Task<List<Walk>> GetAllAsync();
 
-        Task<Walk> GetByIdAsync(Guid id);    
+        Task<Walk?> GetByIdAsync(Guid id);    
     }
 }
