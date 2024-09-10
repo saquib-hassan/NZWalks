39f3c6944e7f6d4d9188b4a80a981be00aa9c36a
Saquib Hassan <hsakib1244@outlook.com>
2024-09-10 18:20:54 +0600

    VForUrRdto

--- NZWalks/NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
@@ -1,8 +1,15 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class UpdateRegionRequestDto
     {
+        [Required]
+        [MinLength(3,ErrorMessage ="Code must be minimum of three characters long")]
+        [MaxLength(3,ErrorMessage ="Code must be maximum of three characters long")]
         public string Code { get; set; }
+        [Required]
+        [MaxLength(50,ErrorMessage ="Name must be maximum of fifty characters long")]
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
     }
