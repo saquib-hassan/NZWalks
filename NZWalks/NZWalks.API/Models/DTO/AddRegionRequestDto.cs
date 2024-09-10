bca3bf0a39e6703ca5754c7eb2368e783da076ee
Saquib Hassan <hsakib1244@outlook.com>
2024-09-10 18:15:44 +0600

    validations added

--- NZWalks/NZWalks.API/Models/DTO/AddRegionRequestDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/AddRegionRequestDto.cs
@@ -1,8 +1,16 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
+
+namespace NZWalks.API.Models.DTO
 {
     public class AddRegionRequestDto
     {
+        [Required]
+        [MinLength(3,ErrorMessage = "Code must be minimum of three characters")]
+        [MaxLength(3, ErrorMessage = "Code must be maximum of three characters")]
         public string Code { get; set; }
+        [Required]
+        [MaxLength(50,ErrorMessage ="Name must be maximum of fifty characters")]
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
     }
