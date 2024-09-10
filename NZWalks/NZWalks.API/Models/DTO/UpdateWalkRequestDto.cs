517f2d0e89463b158445feb970596236f7fe49e1
Saquib Hassan <hsakib1244@outlook.com>
2024-09-10 16:28:37 +0600

    props added

--- NZWalks/NZWalks.API/Models/DTO/UpdateWalkRequestDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/UpdateWalkRequestDto.cs
@@ -2,6 +2,12 @@
 {
     public class UpdateWalkRequestDto
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
 
+        public Guid RegionId { get; set; }
+        public Guid DifficultyID { get; set; }
     }
 }
