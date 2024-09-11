b8d11921ace9e1f342ee86c3d286bde374f1e585
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 17:37:44 +0600

    userManager

--- NZWalks/NZWalks.API/Controllers/AuthController.cs
+++ NZWalks/NZWalks.API/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,10 +9,16 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
 
         [HttpPost]
         [Route("Register")]
-        public async Task<IActionResult> Register()
+        public async Task<IActionResult> Register(RegisterRequestDto registerRequestDto)
         {
 
         }
