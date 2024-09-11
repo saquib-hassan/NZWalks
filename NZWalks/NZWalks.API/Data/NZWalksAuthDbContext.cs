c4176a0e819dbc4f9724f5dbb94ee1852cd20f8b
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 11:58:17 +0600

    customizing db schema

--- NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -8,5 +8,13 @@ namespace NZWalks.API.Data
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerUserRole = "fa6586e9-1b47-40dc-a1c7-dab6be0a8f2d";
+            var writerUserRole = "3d49df08-561c-4cf5-b21a-8934f7e35e1a";
+        }
     }
 }
