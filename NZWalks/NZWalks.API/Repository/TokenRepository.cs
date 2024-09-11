b876c9387da5c4f89ce4bffb095ed10e2b517fa7
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 23:25:32 +0600

    Implemented

---
+++ NZWalks/NZWalks.API/Repository/TokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZWalks.API.Repository
+{
+    public class TokenRepository : ITokenRepository
+    {
+        public string CreateJWTToken(IdentityUser user, List<string> roles)
+        {
+            
+        }
+    }
+}
