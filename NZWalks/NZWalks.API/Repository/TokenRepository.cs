ce5eaa0389cac9483e5d18534808180da4f5d0e0
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 23:26:17 +0600

    List of CLAIM

--- NZWalks/NZWalks.API/Repository/TokenRepository.cs
+++ NZWalks/NZWalks.API/Repository/TokenRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace NZWalks.API.Repository
 {
@@ -6,7 +7,8 @@ namespace NZWalks.API.Repository
     {
         public string CreateJWTToken(IdentityUser user, List<string> roles)
         {
-            
+            var claims = new List<Claim>();
+
         }
     }
 }
