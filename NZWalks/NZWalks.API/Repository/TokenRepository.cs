d330bbbb80a52c8b8735ce7c5d9eb1a48a0d66de
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 23:39:07 +0600

    token configured

--- NZWalks/NZWalks.API/Repository/TokenRepository.cs
+++ NZWalks/NZWalks.API/Repository/TokenRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
@@ -28,6 +29,15 @@ namespace NZWalks.API.Repository
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
 
             var credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                configuration["Jwt:Issuer"],
+                configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
