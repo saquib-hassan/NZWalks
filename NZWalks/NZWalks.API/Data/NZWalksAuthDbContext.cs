c58aa707c13e12332cf78492d28edc9d73f904ce
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 12:02:21 +0600

    added the roles

--- NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -15,6 +16,25 @@ namespace NZWalks.API.Data
 
             var readerUserRole = "fa6586e9-1b47-40dc-a1c7-dab6be0a8f2d";
             var writerUserRole = "3d49df08-561c-4cf5-b21a-8934f7e35e1a";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerUserRole,
+                    Name = "Reader",
+                    ConcurrencyStamp = readerUserRole,
+                    NormalizedName = "Reader".ToUpper()
+                },
+
+                new IdentityRole
+                {
+                    Id=writerUserRole,
+                    Name="Writer",
+                    ConcurrencyStamp=writerUserRole,
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
         }
     }
 }
