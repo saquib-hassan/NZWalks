0423bc48d7d9708120d0fa7e68556b3129af9a17
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 15:21:59 +0600

    database

--- NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,42 +1,42 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 
-namespace NZWalks.API.Data
-{
-    public class NZWalksAuthDbContext : IdentityDbContext
-    {
-        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
-        {
-        }
+//namespace NZWalks.API.Data
+//{
+//    public class NZWalksAuthDbContext : IdentityDbContext
+//    {
+//        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+//        {
+//        }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+//        protected override void OnModelCreating(ModelBuilder builder)
+//        {
+//            base.OnModelCreating(builder);
 
-            var readerUserRole = "fa6586e9-1b47-40dc-a1c7-dab6be0a8f2d";
-            var writerUserRole = "3d49df08-561c-4cf5-b21a-8934f7e35e1a";
+//            var readerUserRole = "fa6586e9-1b47-40dc-a1c7-dab6be0a8f2d";
+//            var writerUserRole = "3d49df08-561c-4cf5-b21a-8934f7e35e1a";
 
-            var roles = new List<IdentityRole>
-            {
-                new IdentityRole
-                {
-                    Id = readerUserRole,
-                    Name = "Reader",
-                    ConcurrencyStamp = readerUserRole,
-                    NormalizedName = "Reader".ToUpper()
-                },
+//            var roles = new List<IdentityRole>
+//            {
+//                new IdentityRole
+//                {
+//                    Id = readerUserRole,
+//                    Name = "Reader",
+//                    ConcurrencyStamp = readerUserRole,
+//                    NormalizedName = "Reader".ToUpper()
+//                },
 
-                new IdentityRole
-                {
-                    Id=writerUserRole,
-                    Name="Writer",
-                    ConcurrencyStamp=writerUserRole,
-                    NormalizedName = "Writer".ToUpper()
-                }
-            };
+//                new IdentityRole
+//                {
+//                    Id=writerUserRole,
+//                    Name="Writer",
+//                    ConcurrencyStamp=writerUserRole,
+//                    NormalizedName = "Writer".ToUpper()
+//                }
+//            };
 
-            builder.Entity<IdentityRole>().HasData(roles);
-        }
-    }
-}
+//            builder.Entity<IdentityRole>().HasData(roles);
+//        }
+//    }
+//}
--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -18,8 +18,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
-builder.Services.AddDbContext<NZWalksAuthDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConnectionString")));
+//builder.Services.AddDbContext<NZWalksAuthDbContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConnectionString")));
 
 builder.Services.AddScoped<IRegionRepository,SqlRegionRepository>();
 builder.Services.AddScoped<IWalkRepository,SqlWalkRepository>();
