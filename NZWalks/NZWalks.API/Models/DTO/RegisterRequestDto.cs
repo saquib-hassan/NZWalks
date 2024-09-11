9556ea6b9a65fb22e1482d1c0ebf728c2a027791
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 17:33:02 +0600

    props and datatype

--- NZWalks/NZWalks.API/Models/DTO/RegisterRequestDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/RegisterRequestDto.cs
@@ -1,6 +1,15 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class RegisterRequestDto
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
     }
 }
