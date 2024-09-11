4a4e664e41643d409c1eab8c93415ecc26597b46
Saquib Hassan <hsakib1244@outlook.com>
2024-09-11 17:34:07 +0600

    prop ready

--- NZWalks/NZWalks.API/Models/DTO/RegisterRequestDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/RegisterRequestDto.cs
@@ -10,6 +10,7 @@ namespace NZWalks.API.Models.DTO
         [Required]
         public string UserName { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
