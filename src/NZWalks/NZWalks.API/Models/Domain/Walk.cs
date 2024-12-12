ebcdf99197d44c5264a04fc6016d40927c67834d
Saquib Hassan <hsakib1244@outlook.com>
2024-12-12 19:40:49 +0600

    i

--- src/NZWalks/NZWalks.API/Models/Domain/Walk.cs
+++ src/NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -5,7 +5,7 @@
         public Guid Id  { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string LegthInKm { get; set; }
+        public double LegthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
         public Guid DifficultyId { get; set; }
         public Guid RegionId { get; set; }
