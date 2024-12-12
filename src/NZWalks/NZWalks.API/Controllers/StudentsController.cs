8c85a41d31b60a7ae91406eb4fcd12b63e861684
Saquib Hassan <hsakib1244@outlook.com>
2024-12-12 19:17:56 +0600

    students are working

---
+++ src/NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = { "Saquib", "Rakib", "Mitu" };
+            return Ok(students);
+        }
+    }
+}
