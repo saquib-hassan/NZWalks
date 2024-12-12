37e6a6b3d2edbdc21cf6c906ca0f8fa09dbcbe32
Saquib Hassan <hsakib1244@outlook.com>
2024-12-12 20:46:08 +0600

    dbContext service1

--- src/NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ src/NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,6 +8,16 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly NZWalksDbContext dbContext;
 
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+        }
     }
 }
