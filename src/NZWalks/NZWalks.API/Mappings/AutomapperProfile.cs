22b2655ff2951243a5d449655695791b1cae217b
Saquib Hassan <hsakib1244@outlook.com>
2024-12-14 22:46:58 +0600

    Profile

---
+++ src/NZWalks/NZWalks.API/Mappings/AutomapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Mappings
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Region,RegionDTO>().ReverseMap();
+        }
+    }
+}
