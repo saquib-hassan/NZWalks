e1622aaa99b15172f706057ae686065082f77da1
Saquib Hassan <hsakib1244@outlook.com>
2024-12-14 20:44:54 +0600

    skeleton implementation

--- src/NZWalks/NZWalks.API/Repositories/SQLRegionRepository.cs
+++ src/NZWalks/NZWalks.API/Repositories/SQLRegionRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace NZWalks.API.Repositories
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
 {
-    public class SQLRegionRepository
+    public class SQLRegionRepository : IRegionRepository
     {
+        private readonly NZWalksDbContext dbContext;
+
+        public SQLRegionRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public Task<Region> CreateAsync(Region region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Region?> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Region> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Region?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Region?> UpdateAsync(Guid id, Region region)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
