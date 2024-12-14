9d509067d577b63dff16616d7fe41b7193d12028
Saquib Hassan <hsakib1244@outlook.com>
2024-12-14 22:48:44 +0600

    AddAutoMapper injected

--- src/NZWalks/NZWalks.API/Program.cs
+++ src/NZWalks/NZWalks.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,7 @@ builder.Services.AddDbContext<NZWalksDbContext>(option =>
 });
 
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+builder.Services.AddAutoMapper(typeof(AutomapperProfile));
 
 var app = builder.Build();
 
