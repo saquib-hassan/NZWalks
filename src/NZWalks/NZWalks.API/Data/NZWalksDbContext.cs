acd6d52fc1180c6379a4aeca24e597900f0db503
Saquib Hassan <hsakib1244@outlook.com>
2024-12-15 19:36:56 +0600

    seeded data of difficulties

--- src/NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
+++ src/NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -17,7 +17,28 @@ namespace NZWalks.API.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            var difficulties = new List<Difficulty>()
+            {
+                new Difficulty
+                {
+                    Id =Guid.Parse("75fac35a-7795-460c-be95-bc89514b6817"),
+                    Name = "Easy"
+                },
+                new Difficulty
+                {
+                    Id =Guid.Parse("7f5e1f21-4f05-45e1-95b5-3d5be3aca5f0"),
+                    Name = "Medium"
+                    
+                },
+                new Difficulty
+                {
+                    Id =Guid.Parse("7887e703-bad7-4c34-be97-112605673f74"),
+                    Name = "Hard"
+                }
+
+            };
+
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
         }
     }
 }
