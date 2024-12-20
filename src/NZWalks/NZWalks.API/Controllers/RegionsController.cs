0bcc3941cf0f6f04d7f3774611368910a3a4970a
Saquib Hassan <hsakib1244@outlook.com>
2024-12-20 17:04:24 +0600

    Authentication/Authorization

--- src/NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ src/NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.CustomValidationFilter;
 using NZWalks.API.Models.Domain;
@@ -9,6 +10,7 @@ namespace NZWalks.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RegionsController : ControllerBase
     {
         private readonly IRegionRepository regionRepository;
