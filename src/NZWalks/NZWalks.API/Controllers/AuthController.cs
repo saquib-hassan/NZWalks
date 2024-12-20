e8f603d522774e19333b18a2a383ea4907ba89d5
Saquib Hassan <hsakib1244@outlook.com>
2024-12-20 19:20:53 +0600

    succesfull user creation

---
+++ src/NZWalks/NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(AddRegisterRequestDto addRegisterRequestDto)
+        {
+            var identityUser = new IdentityUser()
+            {
+                UserName = addRegisterRequestDto.UserName,
+                Email = addRegisterRequestDto.UserName
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, addRegisterRequestDto.Password);
+            if(identityResult.Succeeded)
+            {
+                if(addRegisterRequestDto.Roles != null && addRegisterRequestDto.Roles.Any() )
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, addRegisterRequestDto.Roles);
+               
+
+                if(identityResult.Succeeded)
+                {
+                    return Ok("User was registered! Please login");
+                }
+                }
+            }
+            return BadRequest("Something went wrong");
+        }
+    }
+}
